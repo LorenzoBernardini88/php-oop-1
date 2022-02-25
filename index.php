6db7c24ebe16e613f9c91e4e8234eea5e092d1b7
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-02-25 16:09:09 +0100

    risultato def oop-1

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
-
 <pre>
 <?php
 
 require_once __DIR__.'/classes/Movie.php';
 
+// ISTANZA PRIMO FILM.
 $primoFilm = new Movie("Dune","Denis Villeneuve","Timothée Chalamet");
 $primoFilm->setGenere('Avventura');
 
-
+//ISTANZA SECONDO FILM.
 $secondoFilm = new Movie("Tenet","Christopher Nolan","John David Washington");
 $secondoFilm->setGenere('Fantascienza');
-
-
 
 var_dump($primoFilm);
 var_dump($secondoFilm);
 
-
 ?>
 </pre>
+
+
+
+
 
